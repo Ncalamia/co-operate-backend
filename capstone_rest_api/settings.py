1e4c53cb9f5c174e4decfe94402fa800707793e1
nikki calamia <Ncalamia2468@yahoo.com>
2022-06-30 15:25:05 -0400

    hid secret key

--- capstone_rest_api/settings.py
+++ capstone_rest_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'co-operate-backend.herokuapp.com']
 
